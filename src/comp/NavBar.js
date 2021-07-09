c93b76d03fb711dd219e4050cf8ff3195bec288b
kppalmo <kepalmo8@gmail.com>
2021-07-09 11:12:11 -0400

    fontShadow

--- src/comp/NavBar.js
+++ src/comp/NavBar.js
@@ -12,8 +12,13 @@ font-family:Lato;
     }
     .txtColor{
         color:#FFD700;
+        text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+
     }
-  
+    .txtColor:hover{
+        color:#009dff;
+    }
+ 
 `
 
 function NavigationBar () {
@@ -24,7 +29,7 @@ function NavigationBar () {
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" >
                     <Nav className="ml-auto   ">
-                    <Nav.Item><Nav.Link href="/" >Home</Nav.Link></Nav.Item>
+                    <Nav.Item ><Nav.Link href="/" >Home</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/cast">Cast</Nav.Link></Nav.Item>
                     <Nav.Item><Nav.Link href="/CreativeTeam">Creative Team</Nav.Link></Nav.Item>
--- src/comp/PageBanner.js
+++ src/comp/PageBanner.js
@@ -9,14 +9,23 @@ font-family:Lato;
     background-image: linear-gradient(#009dff, white);
     text-align:center; 
     color:#FFD700;
+    text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
 
+}
+.pagebanner-text{
+    font-size:400%;
+}
+@media screen and (max-width:1020px) {
+    .pagebanner-text{
+        font-size:300%;
+    }
 }
 
 `
 export const Banner = props => (
     <Styles>
         <div class="cont">
-            <h1>ROUND WENT THE WHEEL</h1>
+            <h1 class="pagebanner-text">ROUND WENT THE WHEEL</h1>
             <h3></h3>
         </div>
     </Styles>
